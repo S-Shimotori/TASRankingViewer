56e4bc917f198b55a42621db23817864d423a6e4
S-Shimotori <490.frostybirds@gmail.com>
2016-06-02 00:10:07 +0900

    Add request for last modified #4

--- app/src/main/kotlin/net/terminal_end/tasrankingviewer/activity/MainActivity.kt
+++ app/src/main/kotlin/net/terminal_end/tasrankingviewer/activity/MainActivity.kt
@@ -4,9 +4,12 @@ import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.astuetz.PagerSlidingTabStrip
 import net.terminal_end.tasrankingviewer.R
 import net.terminal_end.tasrankingviewer.fragment.ListFragment
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,5 +24,19 @@ class MainActivity : AppCompatActivity() {
         val pagerSlidingTabStrip = findViewById(R.id.MainViewPagerTab) as PagerSlidingTabStrip
         pagerSlidingTabStrip.setViewPager(viewPager)
         pagerSlidingTabStrip.indicatorColor = Color.rgb(0, 155, 159)
+
+        val client = OkHttpClient()
+        val request = Request.Builder().url(resources.getString(R.string.last_modified)).build()
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call?, e: IOException?) {
+                throw UnsupportedOperationException()
+            }
+
+            override fun onResponse(call: Call?, response: Response?) {
+                if (response != null && response.isSuccessful) {
+                    Log.d("MainActivity", response.body().string())
+                }
+            }
+        })
     }
 }
