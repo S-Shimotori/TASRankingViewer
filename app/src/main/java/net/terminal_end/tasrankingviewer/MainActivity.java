cb01b3016fd6c37f77c6f95f6c8d8f8f72c7aae6
S-Shimotori <490.frostybirds@gmail.com>
2016-06-01 16:26:34 +0900

    Initial commit with Android Studio project

---
+++ app/src/main/java/net/terminal_end/tasrankingviewer/MainActivity.java
@@ -0,0 +1,13 @@
+package net.terminal_end.tasrankingviewer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
