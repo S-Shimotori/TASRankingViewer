eacb81873ed9658895c5c1443700f49e79e66e5c
S-Shimotori <490.frostybirds@gmail.com>
2016-06-02 01:26:20 +0900

    Implement text view for last modified #9

--- app/src/main/kotlin/net/terminal_end/tasrankingviewer/activity/MainActivity.kt
+++ app/src/main/kotlin/net/terminal_end/tasrankingviewer/activity/MainActivity.kt
@@ -4,12 +4,16 @@ import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.widget.TextView
 import com.astuetz.PagerSlidingTabStrip
+import com.google.gson.Gson
 import net.terminal_end.tasrankingviewer.R
 import net.terminal_end.tasrankingviewer.fragment.ListFragment
+import net.terminal_end.tasrankingviewer.model.LastModified
+import net.terminal_end.tasrankingviewer.util.toCalendar
 import okhttp3.*
 import java.io.IOException
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,17 +30,39 @@ class MainActivity : AppCompatActivity() {
         pagerSlidingTabStrip.indicatorColor = Color.rgb(0, 155, 159)
 
         val client = OkHttpClient()
-        val request = Request.Builder().url(resources.getString(R.string.last_modified)).build()
+        val request = Request.Builder().url(resources.getString(R.string.endpoint_last_modified)).build()
+        val lastModifiedTextView = findViewById(R.id.LastModifiedTextView) as TextView
         client.newCall(request).enqueue(object: Callback {
             override fun onFailure(call: Call?, e: IOException?) {
-                throw UnsupportedOperationException()
+                lastModifiedTextView.text = resources.getString(R.string.fail)
             }
 
             override fun onResponse(call: Call?, response: Response?) {
                 if (response != null && response.isSuccessful) {
-                    Log.d("MainActivity", response.body().string())
+                    val lastModified = Gson().fromJson(response.body().string(), LastModified::class.java)
+                    val calendarResult = lastModified.last_modified.toCalendar()
+                    if (calendarResult.component1() != null) {
+                        lastModifiedTextView.text = createLastModifiedMessage(calendarResult.get())
+                        return
+                    }
                 }
+                lastModifiedTextView.text = resources.getString(R.string.fail)
             }
         })
+    }
+
+    fun createLastModifiedMessage(calendar: Calendar): String {
+        return calendar.get(Calendar.YEAR).toString() +
+                resources.getString(R.string.year) +
+                (calendar.get(Calendar.MONTH) + 1).toString() +
+                resources.getString(R.string.month) +
+                calendar.get(Calendar.DATE).toString() +
+                resources.getString(R.string.date) +
+                calendar.get(Calendar.HOUR_OF_DAY).toString() +
+                resources.getString(R.string.hour) +
+                calendar.get(Calendar.MINUTE).toString() +
+                resources.getString(R.string.minute) +
+                calendar.get(Calendar.SECOND).toString() +
+                resources.getString(R.string.second)
     }
 }
