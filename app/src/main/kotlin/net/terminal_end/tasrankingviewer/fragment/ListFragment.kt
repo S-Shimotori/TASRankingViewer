593ea471419a90b715d1e49503a7c7d6b0acc173
S-Shimotori <490.frostybirds@gmail.com>
2016-06-03 00:35:54 +0900

    Add request to search #4

--- app/src/main/kotlin/net/terminal_end/tasrankingviewer/fragment/ListFragment.kt
+++ app/src/main/kotlin/net/terminal_end/tasrankingviewer/fragment/ListFragment.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
 import android.widget.Toast
+import com.google.gson.Gson
 import net.terminal_end.tasrankingviewer.R
+import net.terminal_end.tasrankingviewer.model.SearchQuery
 import net.terminal_end.tasrankingviewer.widget.ListItemAdapter
+import okhttp3.*
+import java.io.IOException
 
 /**
  * Created by S-Shimotori on 6/1/16.
@@ -37,7 +42,43 @@ class ListFragment: Fragment() {
             Toast.makeText(activity, str, Toast.LENGTH_SHORT).show()
         }
 
+        update()
+
         return listView
+    }
+
+    fun update() {
+        val query = SearchQuery.getInstance(
+                "TAS OR TAP OR tool\"-\"",
+                SearchQuery.SearchField.TAG,
+                listOf(SearchQuery.Field.cmsid, SearchQuery.Field.title, SearchQuery.Field.view_counter),
+                listOf(SearchQuery.Filter.Range.String(
+                        SearchQuery.Field.start_time,
+                        "2016-05-01 00:00:00", "2016-05-31 23:59:59", true, true
+                )),
+                SearchQuery.SortBy.view_counter, SearchQuery.Order.desc, 0, 5
+        )!!
+        val jsonString = Gson().toJson(query)
+
+        Log.d("ListFragment", jsonString)
+
+        val client = OkHttpClient()
+        val request = Request.Builder()
+                .url(resources.getString(R.string.endpoint_search))
+                .post(RequestBody.create(MediaType.parse("application/json; charset=utf-8"), jsonString))
+                .build()
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call?, e: IOException?) {
+                Log.e("ListFragment", e.toString())
+            }
+
+            override fun onResponse(call: Call?, response: Response?) {
+                if (response != null && response.isSuccessful) {
+                    val jsonString = response.body().string()
+                    Log.d("ListFragment", jsonString)
+                }
+            }
+        })
     }
 
     class ListFragmentPagerAdapter(fm: FragmentManager, titles: List<String>): FragmentPagerAdapter(fm) {
